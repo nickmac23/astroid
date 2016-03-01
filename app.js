62cb6fbb73d8788a71db7a04e926373a20e00c62
nickmac <nickpmacintyre@gmail.com>
2016-02-29 18:32:35 -0700

    bouncing astriods

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 ctx.canvas.width = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
-console.log(ctx.canvas.width);
 
 var ship1 = new Image();
   ship1.src = 'pic/blueships1.png';
 var bullet1 = new Image();
   bullet1.src = 'pic/Bluecenter.png';
+var astroid = new Image();
+  astroid.src = 'pic/Astromedium.png'
 
 var Ship = {
   x: canvas.width/2,
@@ -27,6 +28,48 @@ var Ship = {
     ctx.restore();
   }
 }
+function Asteroids (){
+  this.x = 0;
+  this.y = 0;
+  this.vx = 1;
+  this.vy = 1;
+  this.start = function () {
+    this.x = Math.random() * (canvas.width - 0);
+    this.y = Math.random() * (canvas.height - 0);
+    var check = Math.floor(Math.random() * 4);
+    console.log(check);
+    if(check === 0){
+      this.y = 0 + 300;
+      this.vy = 2;
+    }if(check === 2){
+      this.y = canvas.height - 300;
+      this.vy = -2;
+    }if(check === 3){
+      this.x = 0 + 300;
+      this.vx = 2;
+    }if(check === 1){
+      this.x = canvas.width - 300;
+      this.vx = -2;
+    };
+  };
+  this.draw = function () {
+    this.x += this.vx;
+    this.y += this.vy;
+    this.bounce();
+    ctx.drawImage(astroid, this.x , this.y )
+  }
+  this.bounce = function () {
+    if( this.x + astroid.width > canvas.width || this.x < 0){
+      this.vx = -this.vx;
+    }
+    if( this.y + astroid.height > canvas.height || this.y < 0){
+      this.vy = -this.vy;
+    }
+  }
+}
+Astroid1 = new Asteroids ();
+Astroid2 = new Asteroids ();
+Astroid3 = new Asteroids ();
 
 function Bullets (positionX, positionY, angle){
   this.x = positionX;
@@ -44,7 +87,6 @@ function Bullets (positionX, positionY, angle){
   this.draw = function (){
     this.x += this.dv * Math.sin(this.angle);
     this.y -= this.dv * Math.cos(this.angle);
-  
     ctx.drawImage(bullet1, this.x - 25 , this.y - 10)
   }
 };
@@ -94,8 +136,14 @@ function move (key) {
     if( Bullet1.val ){
       Bullet1.draw()
     }
+    Astroid1.draw();
+    Astroid2.draw();
+    Astroid3.draw();
     Ship.draw();
     window.requestAnimationFrame(gameLogic);
   }
+  Astroid1.start();
+  Astroid2.start();
+  Astroid3.start();
   gameLogic();
   //window.requestAnimationFrame(gameLogic);
